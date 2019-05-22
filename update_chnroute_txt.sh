1af45088bcdcd1bd0d0372a1a9f94c39d698f9fc
Charles Chen <wancaibida@gmail.com>
2019-05-22 20:52:12 +0800

    update

--- update_chnroute_txt.sh
+++ update_chnroute_txt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/bin/wget -O- https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt > /root/scripts/chnroute.txt
+/bin/wget -O- https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt > /root/scripts/china_ip_list.txt

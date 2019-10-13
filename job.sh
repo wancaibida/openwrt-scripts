5e2ef9fca4ef6403091d17460ac35c0c4e00c747
wancaibida <wancaibida@gmail.com>
2019-10-13 22:16:07 +0800

    update script

--- job.sh
+++ job.sh
@@ -6,10 +6,10 @@
 ## update dnsmasq ad block
 /root/scripts/update_dnsmasq_adblock.sh
 
-## /etc/init.d/dnsmasq restart
+/etc/init.d/dnsmasq restart
 
-## update chnroute
-/root/scripts/update_china_ip_list.sh
+## update chnroute for iptables
+# /root/scripts/update_china_ip_list.sh
 
 ## update aria2 tracker 
 #/root/scripts/update_aria2_tracker.sh

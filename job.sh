e9aad03b815326af51c7e389529cc547508c270f
Charles Chen <wancaibida@gmail.com>
2019-12-06 09:43:03 +0000

    update scripts

--- job.sh
+++ job.sh
@@ -4,12 +4,12 @@
 /root/scripts/update_dnsmasq_china.sh
 
 ## update dnsmasq ad block
-/root/scripts/update_dnsmasq_adblock.sh
+## /root/scripts/update_dnsmasq_adblock.sh
 
 /etc/init.d/dnsmasq restart
 
 ## update chnroute for iptables
-# /root/scripts/update_china_ip_list.sh
+## /root/scripts/update_china_ip_list.sh
 
 ## update aria2 tracker 
-#/root/scripts/update_aria2_tracker.sh
+## /root/scripts/update_aria2_tracker.sh
